66abfc69f38c4df8753ce0c2b622ea7aed74d8f3
unknown <victoria.borges027@gmail.com>
2025-03-20 11:45:42 -0300

    Pacotes criados

---
+++ src/main/java/com/generation/delivery/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.generation.delivery.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.delivery.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+
+
+}
